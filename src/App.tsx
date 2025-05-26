71f45e828b7eb991313b794e60023ea098ba40a4
soshi04 <sirsohumjoshi@gmail.com>
2025-05-26 17:10:55 -0500

    routing between pictest and main

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import './App.css'
+import PicTest from './pages/PicTest'
+import MainPage from './pages/MainPage'
 
 /**
  * This defines our React functional component called App.
@@ -17,65 +20,12 @@ function MyOtherButton(){
   )
 }
 function App() {
-  let count: number = 4;
-  let active: boolean = true;
-
-  /**
-   * use state always needs to be in the form  const [value, setter]
-   * value → the current data
-   * setValue → a function to change it (and re-render the component)
-   */
-  const [index, setIndex] = useState<number>(0);
-  const [name, setName] = useState<string>("")
-  const [tempName, setTempName] = useState<string>("")
-
-
-  const handleClick = () => {
-    setIndex((curr) => (curr + 1) % 3)
-  }
-
-  const currentValue = () => {
-    if (index == 0) return `Count is: ${count}`
-    if (index == 1) return `Name is: ${name}`
-    if (index == 2) return `Active status: ${active}`
-  }
-
-  const change_name = () => {
-  }
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTempName(e.target.value)
-  }
-
-  const pressedEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      setName(tempName);
-      setTempName("");
-
-    }
-  }
-
-  return (
-  <div className="App">
-    <h1>{currentValue()}</h1>
-    <button onClick={handleClick}>Cycle</button>
-    <p>input name</p>
-    <input 
-    type="text"
-    value={tempName}
-    onChange={handleInputChange}
-    onKeyDown={pressedEnter}
-    placeholder='type name'
-    />
-    {
-      name == "sohum" ? (
-        <MyButton/>
-      ) : (
-        <MyOtherButton/>
-      )}
-
-  </div>
-  );
+  return(
+    <Routes>
+    <Route path="/" element={<MainPage />} />
+    <Route path="/PicTest" element={<PicTest />} />
+  </Routes>
+  )
 }
 
 export default App
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,67 @@
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+
+
+/**
+ * This defines our React functional component called MainPage.
+ * React components are functions that !! return JSX !! (the stuff that renders on the page).
+ */
+function MainPage() {
+  let count: number = 4;
+  let active: boolean = true;
+
+  /**
+   * use state always needs to be in the form  const [value, setter]
+   * value → the current data
+   * setValue → a function to change it (and re-render the component)
+   */
+  const [index, setIndex] = useState<number>(0);
+  const [name, setName] = useState<string>("")
+  const [tempName, setTempName] = useState<string>("")
+
+
+  const handleClick = () => {
+    setIndex((curr) => (curr + 1) % 3)
+  }
+
+  const currentValue = () => {
+    if (index == 0) return `Count is: ${count}`
+    if (index == 1) return `Name is: ${name}`
+    if (index == 2) return `Active status: ${active}`
+  }
+
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTempName(e.target.value)
+  }
+
+
+  const pressedEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      setName(tempName);
+      setTempName("");
+    }
+  }
+
+  const navigate = useNavigate()
+
+  return (
+  <div className="App">
+    <h1>{currentValue()}</h1>
+    <div>
+    <button onClick={handleClick}>Cycle</button>
+    <input 
+    type="text"
+    value={tempName}
+    onChange={handleInputChange}
+    onKeyDown={pressedEnter}
+    placeholder='type name'
+    />
+    </div>
+    <button onClick={() => navigate('/PicTest')}>Go to picstest</button>
+
+  </div>
+  );
+}
+
+export default MainPage
