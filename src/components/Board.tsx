c994a6248da74a95ae008f1f1fa8173d3e268921
soshi04 <sirsohumjoshi@gmail.com>
2025-05-27 17:46:32 -0500

    fixed board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,43 +1,47 @@
 import "./tiktaktoe.css"
 import { useState } from 'react'
 
+type SquaresProp = {
+    value: string | null;
+    onSquareClick : () => void;
+};
 
-function Square(){
-    const [state, setState] = useState<string | null>(null)
-    /*
-    the syntax above tells typescript that state can be either
-    null or type string.
-    */
-    function handleClick () {
-        setState('X')
-    }
+
+function Square( {value, onSquareClick}:SquaresProp ){
     return (
-        <button className="square" onClick={handleClick}>{state}</button>
+        <button className="square" onClick={onSquareClick}>{value}</button>
     )
 }
 
-
-
-
 export default function Board() {
+    const [squares, setSquares] = useState<(string | null)[]>(Array(25).fill(null));
+    function handleClick(i:number) {
+        const nextSquares = squares.slice();
+        nextSquares[i] = "X";
+        setSquares(nextSquares);
+    }
+
+    const board = [];
+    for (let row = 0; row < 5; row++) {
+        const rowSquares = [];
+        for (let col = 0; col < 5; col++) {
+            const index = row * 5 + col;
+            rowSquares.push(
+                <Square
+                    key={index}
+                    value={squares[index]}
+                    onSquareClick={() => handleClick(index)}
+                />
+            );
+        }
+        board.push(<div className="board-row" key={row}>{rowSquares}</div>);
+    }
+    /*
+    the syntax above tells typescript that state for the array can be either null or type string. 
+    */
     return (
         <>
-        <div className="board-row">
-            <Square/>
-            <Square />
-            <Square />
-        </div>
-        <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
-        </div>
-        <div className="board-row">
-            <Square />
-            <Square />
-            <Square />
-        </div>
-
+        {board}
         </>
 
     )
